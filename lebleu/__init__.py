bfb8efe8f877599ce8f092b42db3c196e5fbcd64
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2015-04-30 11:14:25 +0300

    Initial commit. My fork of harry as submodule.

---
+++ lebleu/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+LeBLEU - Letter-edit / Levenshtein BLEU
+"""
+import logging
+
+
+#__all__ = []
+
+__version__ = '0.0.1'
+__author__ = 'Stig-Arne Gronroos'
+__author_email__ = "stig-arne.gronroos@aalto.fi"
+
+_logger = logging.getLogger(__name__)
+
+
+def get_version():
+    return __version__
+
+# The public api imports need to be at the end of the file,
+# so that the package global names are available to the modules
+# when they are imported.
+
+#from .lebleu import FIXME
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+import re
+main_py = open('lebleu/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
+
+#requires = [ ]
+
+setup(name='lebleu',
+      version=metadata['version'],
+      author=metadata['author'],
+      author_email='stig-arne.gronroos@aalto.fi',
+      #url='',
+      description='lebleu',
+      packages=['lebleu', 'lebleu.tests'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+      ],
+      license="BSD",
+      #scripts=['scripts/lebleu',
+      #        ],
+      #install_requires=requires,
+      #extras_require={
+      #    'docs': [l.strip() for l in open('docs/build_requirements.txt')]
+      #}
+      )
